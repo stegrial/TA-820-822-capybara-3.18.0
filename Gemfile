bda9abcf25195469bfc0ae52ef18def6a44b4376
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-05-06 17:42:58 +0300

    adding tests for special methods of Capybara 3.18.0 + TA

--- Gemfile
+++ Gemfile
@@ -3,9 +3,11 @@ source 'https://rubygems.org'
 gem 'rake'
 
 group(:test) do
-  gem 'true_automation', git: 'https://github.com/educator-io/trueautomation-libraries', branch: 'fix_capybara_new_driver', glob: 'ruby/true_automation/true_automation.gemspec'
+  # gem 'true_automation', git: 'https://github.com/educator-io/trueautomation-libraries', branch: 'fix_capybara_new_driver', glob: 'ruby/true_automation/true_automation.gemspec'
+  gem 'true_automation', git: '/Users/stegrial/Documents/TrueAutomation/lib/trueautomation-libraries', branch: 'master', glob: 'ruby/true_automation/true_automation.gemspec'
   # gem 'true_automation'
   gem 'capybara-screenshot'
+  # gem 'capybara', '3.16.0'
   gem 'capybara'
   gem 'rspec'
   gem 'single_test'
--- spec/test_scenario/trueautomation_spec.rb
+++ spec/test_scenario/trueautomation_spec.rb
@@ -8,7 +8,59 @@ feature 'TrueAutomation.IO capybara example' do
 
     find(:css, ta('trueautomationio:signin:signupBtn', 'div.sign-up-container > a')).click
 
-    fill_in ta('trueautomationio:signup:email', 'email'), with: 'your@mail.com'
+    fill_in 'email', with: 'your@mail.com'
+
+    sleep 3
+  end
+
+  scenario 'Test example' do
+    visit 'https://app.trueautomation.io/auth/signin'
+
+    click_button(ta('within:element_inside:click_button', 'Login'))
+
+    sleep 3
+  end
+
+  scenario 'Test example 14 - select id' do
+      visit 'https://www.facebook.com'
+
+        select('11', from: ta('within:element_inside:select', 'day'))
+
+      sleep 3
+  end
+
+  scenario 'Test example 15 - choose text' do
+      visit 'https://www.facebook.com'
+
+        choose(ta('within:element_inside:choose', 'Мужчина'))
+
+      sleep 3
+  end
+
+  scenario 'Test example 8 - click_link and two elements on the page (first element)' do
+
+      visit 'https://trueautomation.io/'
+
+      click_link(ta('within:element_inside:documentation', 'Documentation'))
+
+      sleep 3
+  end
+
+  scenario 'Test example 8 - click_link and two elements on the page (first element)' do
+
+    visit 'https://app.trueautomation.io/auth/signin'
+
+    click_link(ta('within:element_inside:back_to_main', 'Back to main'))
+
+    sleep 3
+  end
+
+  scenario 'Test example 8 - click_link and two elements on the page (first element)' do
+
+    visit 'https://app.trueautomation.io/auth/signin'
+
+    click_link(ta('within:element_inside:forgot_pass', 'Forgot your password?'))
+
     sleep 3
   end
 end
